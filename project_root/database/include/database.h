67e70ffde47f574b0106b447986cbd862912d87d
roberto ingenito <ingenitoroby@gmail.com>
2024-07-12 09:49:58 +0200

    fix

--- project_root/database/include/database.h
+++ project_root/database/include/database.h
@@ -1,17 +1,15 @@
-#include <libpq-fe.h> // sudo apt-get install libpq-dev
+#include <libpq-fe.h>  // sudo apt-get install libpq-dev
 
 typedef struct {
-    char *user; 
-    char *password; 
-    char *dbname; 
-    char *hostaddr; 
-    int port; 
-} database_connection_info; 
+    char *user;
+    char *password;
+    char *dbname;
+    char *hostaddr;
+    int port;
+} database_connection_info;
 
+void connect_database(const char *conninf);
+void disconnect_database();
+void execute_query(const char *query);
 
-void connect_database(const char *conninf); 
-void disconnect_database(); 
-void execute_query(const char *query); 
-
-// altre funzioni specifiche ... 
-
+// altre funzioni specifiche ...
--- project_root/database/src/database.c
+++ project_root/database/src/database.c
@@ -1,30 +1,29 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-
 #include "database.h"
 
-static PGconn = *connection = NULL; 
+#include <stdio.h>
+#include <stdlib.h>
 
-void disconnect_database(){
-    PQfinish(connection); 
+static PGconn *connection = NULL;
+
+void disconnect_database() {
+    PQfinish(connection);
 }
 
-void connect_database(const char *conninfo){
-    connection = PQconnectdb(conninfo); 
-    if(PQstatus(conn) != CONNECTION_OK){
-        print(stderr, "Connection to database failed: %s\n", PQerrorMessage(connection)); 
-        disconnect_database(); 
-        exit(EXIT_FAILURE); 
+void connect_database(database_connection_info *conninfo) {
+    connection = PQconnectdb(conninfo);
+    if (PQstatus(conn) != CONNECTION_OK) {
+        print(stderr, "Connection to database failed: %s\n", PQerrorMessage(connection));
+        disconnect_database();
+        exit(EXIT_FAILURE);
     }
 }
 
-void execute_query(const char *query){
-    PGresult = *result = PQexec(connection, query); 
-    if(PQresultStatus(result) != PGRES_COMMAND_OK){
-        print(stderr, "Failed to execute Query: %s \n", PQerrorMessage(connection)); 
-        PQclear(result); 
-        exit(EXIT_FAILURE); 
+void execute_query(const char *query) {
+    PGresult *result = PQexec(connection, query);
+    if (PQresultStatus(result) != PGRES_COMMAND_OK) {
+        print(stderr, "Failed to execute Query: %s \n", PQerrorMessage(connection));
+        PQclear(result);
+        exit(EXIT_FAILURE);
     }
-    PQclear(result); 
+    PQclear(result);
 }
-
