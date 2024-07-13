6671092bd51cf3d4b045c1be46f936c6a0099ef9
Simone <sim.ingenito@gmail.com>
2024-07-13 16:00:04 +0200

    Funzioni login e signup

--- project_root/database/include/database.h
+++ project_root/database/include/database.h
@@ -4,15 +4,8 @@
 
 static PGconn *connection = NULL;
 
-typedef struct {
-    char **values;
-    int rows;
-    int fields_number;
-} query_result;
-
 void connect_database(char *db_info);
 void disconnect_database();
-void execute_query(const char *query);
 
 /* returns 0 if no error occurred, 1 otherwise */
 
@@ -24,17 +17,8 @@ int search_avaiable_books(PGresult **res);
 int search_books_by_genre(PGresult **res, const char *book_genre);
 int search_books_by_name(PGresult **res, const char *book_name);
 
-// INSERT
-int create_loan(const char *ISBN, const char *user_id);
 
-// DELETE
-int delete_loan(const char *ISBN, const char *user_id);
-
-/*
-    FUNZIONE TIPO :
-        1) eseguo get_books()
-        2) ottengo un db_status
-        3)
+int create_loan(const char *ISBN, const char *username);
 
 
-*/
+int update_loan(const char *ISBN, const char *username);
--- project_root/server/src/main.c
+++ project_root/server/src/main.c
@@ -3,19 +3,23 @@
 int main(int argc, char const *argv[]) {
     connect_database("dbname=library user=postgres password=admin host=localhost port=5432");
 
-    PGresult *res;
 
-    get_books(&res);
+    int result = login("Simoone", "mypsw");
 
-    // Stampa le righe del risultato
-    for (int i = 0; i < PQntuples(res); i++) {
-        for (int j = 0; j < PQnfields(res); j++) {
-            printf("%s\t", PQgetvalue(res, i, j));
-        }
-        printf("\n");
-    }
+    printf("\nQuery Result: %d \n", result);
+    // PGresult *res;
 
-    PQclear(res);
+    // get_books(&res);
+
+    // // Stampa le righe del risultato
+    // for (int i = 0; i < PQntuples(res); i++) {
+    //     for (int j = 0; j < PQnfields(res); j++) {
+    //         printf("%s\t", PQgetvalue(res, i, j));
+    //     }
+    //     printf("\n");
+    // }
+
+    // PQclear(res);
 
     disconnect_database();
 
