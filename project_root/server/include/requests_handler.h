d757a7d9b8147ebd2580ef710852c56b85a6bb5f
roberto ingenito <ingenitoroby@gmail.com>
2024-07-18 11:25:17 +0200

    ora il thread salva nel suo spazio di memoria, lo username del client connesso

--- project_root/server/include/requests_handler.h
+++ project_root/server/include/requests_handler.h
@@ -4,8 +4,8 @@
 #include "../../config/request_config.h"
 #include "../../database/include/database.h"
 
-void handle_signup(int client_socket);
-void handle_signin(int client_socket);
+void handle_signup(int client_socket, char *username);
+void handle_signin(int client_socket, char *username);
 void handle_explore_catalog(int client_socket);
 void handle_search_available_books(int client_socket);
 void handle_search_book_by_name(int client_socket);
